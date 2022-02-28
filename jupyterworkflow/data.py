7a3473c1e715f982fc172308d8f065f16343fddb
VishnuPrabhas <Vishnu.Prabhas@powerschool.com>
2022-02-28 17:27:47 +0530

    refactor data download

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -23,5 +23,10 @@ def load_data(filename='Fremont.csv', url=FREEMONT_URL, force_download=False):
     """
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+    # pd.to_datetime(data.index) #again takes long time
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
     return data
