2ac2ae8391bea6f8ee681f26a8de6433163650a3
VishnuPrabhas <Vishnu.Prabhas@powerschool.com>
2022-02-28 17:46:02 +0530

    BUG: fix in data.py date parsing

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -26,7 +26,7 @@ def load_data(filename='Fremont.csv', url=FREEMONT_URL, force_download=False):
     data = pd.read_csv('Fremont.csv', index_col='Date')
     # pd.to_datetime(data.index) #again takes long time
     try:
-        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
     except TypeError:
         data.index = pd.to_datetime(data.index)
     return data
